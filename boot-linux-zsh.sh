cc4a41255cb98181aa14e361aa28cdb8af978805
André Gomes <andre.goncalves@genesis-ai.company>
2026-02-12 16:26:05 -0800

    Make installation target this branch

--- boot-linux-zsh.sh
+++ boot-linux-zsh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-OMAKUB_PATH="$HOME/.local/share/omakub"
+OMAKUB_PATH="$HOME/.local/share/omakub/no_sudo_installation"
 git clone https://github.com/AndruGomes13/omakub "$OMAKUB_PATH" 2>/dev/null || git -C "$OMAKUB_PATH" pull
 exec "$OMAKUB_PATH/install-linux-zsh.sh"
