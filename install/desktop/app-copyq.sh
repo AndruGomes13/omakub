850270a02e927821e4dd1ff85361a2163d205e43
André Gomes <agoncalve@ethz.ch>
2026-06-01 15:30:07 +0200

    Revert copyq changes

--- install/desktop/app-copyq.sh
+++ install/desktop/app-copyq.sh
@@ -3,7 +3,6 @@
 # Install CopyQ clipboard manager
 sudo apt install -y copyq
 
-# Set up autostart with GTK platform theme so CopyQ follows GNOME dark mode
+# Set up autostart
 mkdir -p ~/.config/autostart
 cp /usr/share/applications/com.github.hluk.copyq.desktop ~/.config/autostart/
-sed -i 's/^Exec=copyq/Exec=env QT_QPA_PLATFORMTHEME=gtk3 copyq/' ~/.config/autostart/com.github.hluk.copyq.desktop
