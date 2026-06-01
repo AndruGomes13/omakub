8eca5a3027984dc9d42be0c1946c8d3a5bf8ef39
André Gomes <agoncalve@ethz.ch>
2026-06-01 15:02:46 +0200

    Fix notion

--- install/desktop/app-notion-webapp.sh
+++ install/desktop/app-notion-webapp.sh
@@ -3,4 +3,3 @@
 # Create Notion web app
 source $OMAKUB_PATH/defaults/bash/functions
 web2app 'Notion' https://www.notion.so/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/notion.png
-app2folder 'Notion.desktop' WebApps
