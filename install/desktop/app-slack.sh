585e697ff9ca85320436e02877d694b2573ba462
André Gomes <agoncalve@ethz.ch>
2026-06-01 13:59:07 +0200

    Add slack install

---
+++ install/desktop/app-slack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Team messaging using https://slack.com
+cd /tmp
+wget -O slack.deb "https://slack.com/downloads/instructions/linux?ddl=1&build=deb"
+sudo apt install -y ./slack.deb
+rm slack.deb
+cd -
