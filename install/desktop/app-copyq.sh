5e769bfad4b67f2793f80f42cda80569acfb8c22
André Gomes <agoncalve@ethz.ch>
2026-06-01 15:26:06 +0200

    Make copyq dark theme

--- install/desktop/app-copyq.sh
+++ install/desktop/app-copyq.sh
@@ -3,6 +3,7 @@
 # Install CopyQ clipboard manager
 sudo apt install -y copyq
 
-# Set up autostart
+# Set up autostart with GTK platform theme so CopyQ follows GNOME dark mode
 mkdir -p ~/.config/autostart
 cp /usr/share/applications/com.github.hluk.copyq.desktop ~/.config/autostart/
+sed -i 's/^Exec=copyq/Exec=env QT_QPA_PLATFORMTHEME=gtk3 copyq/' ~/.config/autostart/com.github.hluk.copyq.desktop
